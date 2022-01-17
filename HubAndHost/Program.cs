3bdd7acfb3d699494838adad1cf03b7066be4701
Kevin Pilch <me@pilchie.com>
2022-01-16 18:41:43 -0800

    Get more docker working

--- HubAndHost/Program.cs
+++ HubAndHost/Program.cs
@@ -3,6 +3,7 @@ using Orleans.Configuration;
 using Orleans;
 using Snakes;
 using Microsoft.AspNetCore.SignalR;
+using System.Net;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddSignalR();
@@ -15,6 +16,7 @@ builder.Services.AddSingleton(client);
 var app = builder.Build();
 
 app.UseResponseCompression();
+
 app.MapGet("/", () => "Hello World!");
 app.MapHub<SnakeHub>("/snakehub");
 app.Run();
@@ -23,15 +25,18 @@ static async Task<IClusterClient> ConnectClient()
 {
     IClusterClient client;
     client = new ClientBuilder()
-        .UseLocalhostClustering()
+        .UseStaticClustering(
+            new IPEndPoint(IPAddress.Loopback, 30000)
+        )
         .Configure<ClusterOptions>(options =>
         {
             options.ClusterId = "dev";
             options.ServiceId = "Snakes";
         })
-        .ConfigureLogging(logging => logging.SetMinimumLevel(LogLevel.Warning).AddConsole())
+        .ConfigureLogging(logging => logging/*.SetMinimumLevel(LogLevel.Warning)*/.AddConsole())
         .Build();
 
+    Console.WriteLine("Client about to connect to silo host \n");
     await client.Connect();
     Console.WriteLine("Client successfully connected to silo host \n");
     return client;
--- SnakesSilo/Program.cs
+++ SnakesSilo/Program.cs
@@ -3,16 +3,19 @@ using Orleans;
 using Orleans.Configuration;
 using Orleans.Hosting;
 using Snakes;
+using System.Net;
 
 try
 {
     var host = await StartSilo();
-    Console.WriteLine("\n\n Press Enter to terminate...\n\n");
-    Console.ReadLine();
+    //Console.WriteLine("\n\n Press Enter to terminate...\n\n");
+    //Console.ReadLine();
 
-    await host.StopAsync();
-
-    return 0;
+    Console.CancelKeyPress += (s,e) => host.StopAsync(); 
+    while (true)
+    {
+        await Task.Delay(TimeSpan.FromSeconds(1));
+    }
 }
 catch (Exception ex)
 {
@@ -24,14 +27,15 @@ static async Task<ISiloHost> StartSilo()
 {
     // define the cluster configuration
     var builder = new SiloHostBuilder()
-        .UseLocalhostClustering()
+        .UseDevelopmentClustering(new IPEndPoint(IPAddress.Loopback, 11111))
+        .ConfigureEndpoints(IPAddress.Loopback, siloPort: 11111, gatewayPort: 30000, listenOnAnyHostAddress: true)
         .Configure<ClusterOptions>(options =>
         {
             options.ClusterId = "dev";
             options.ServiceId = "Snakes";
         })
         .ConfigureApplicationParts(parts => parts.AddApplicationPart(typeof(PlayerGrain).Assembly).WithReferences())
-        .ConfigureLogging(logging => logging.SetMinimumLevel(LogLevel.Warning).AddConsole());
+        .ConfigureLogging(logging => logging/*.SetMinimumLevel(LogLevel.Warning)*/.AddConsole());
 
     var host = builder.Build();
     await host.StartAsync();
